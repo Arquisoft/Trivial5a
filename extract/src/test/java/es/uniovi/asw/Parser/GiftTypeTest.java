1651a17086d55c2e60b9d29963892c83fa83d469
changqu <changquye@yahoo.es>
2015-02-27 16:55:01 +0100

    prueba de changqu
    Former-commit-id: b4605d7c04366e623aea3b8ee00bd4fc9262e486

--- extract/src/test/java/es/uniovi/asw/Parser/GiftTypeTest.java
+++ extract/src/test/java/es/uniovi/asw/Parser/GiftTypeTest.java
@@ -5,17 +5,39 @@ package es.uniovi.asw.Parser;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 import org.junit.Test;
 
 /**
- * @author ikzer
+ * @author changqu
  *
  */
 public class GiftTypeTest {
 
+	GiftType gt = new GiftType();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws IOException {
+		Document file = new Document("src/main/java/es/uniovi/asw/data/preguntas.gift");
+		
+		Parser parser = new Parser();
+		
+		String leido = new String();
+
+		BufferedReader bf = new BufferedReader(new FileReader(file));
+		while (bf.ready()) {
+			leido += bf.readLine() + "\n";
+		}
+		parser.lineas = leido.split("[\r\n]");
+		bf.close();
+		
+		assertEquals(true, gt.verify(parser.lineas));
 	}
 
 }
--- extract/src/test/java/es/uniovi/asw/model/CategoryTest.java
+++ extract/src/test/java/es/uniovi/asw/model/CategoryTest.java
@@ -8,14 +8,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
- * @author ikzer
+ * @author changqu
  *
  */
 public class CategoryTest {
-
+	
+	Category category = new Category();
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		category.setName("primer pregunta");
+		Question question = new Question();
+		question.addWrongAnswer("Lunes", 0);
+		question.addWrongAnswer("Martes",1);
+		question.setCorrectAnswer("Domingo");
+		question.setIdentifer("uno");
+		question.setQuery("q1");
+		category.addQuestions(question);
+		assertEquals("Category [name=primer pregunta, "
+				+ "questions=[Question [identifier= uno, query=q1, correctAnswer=Domingo, "
+				+ "wrongAnswers=[Lunes, Martes]]]]",category.toString());
 	}
 
 }
