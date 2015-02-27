b4605d7c04366e623aea3b8ee00bd4fc9262e486
changqu <changquye@yahoo.es>
2015-02-27 16:55:01 +0100

    prueba de changqu

--- extract/src/test/java/es/uniovi/asw/model/QuestionTest.java
+++ extract/src/test/java/es/uniovi/asw/model/QuestionTest.java
@@ -8,14 +8,22 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
- * @author ikzer
+ * @author changqu
  *
  */
 public class QuestionTest {
 
+	Question question = new Question();
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		question.addWrongAnswer("Lunes", 0);
+		question.addWrongAnswer("Martes",1);
+		question.setCorrectAnswer("Domingo");
+		question.setIdentifer("uno");
+		question.setQuery("q1");
+		assertEquals("Question [identifier= uno, query=q1, correctAnswer=Domingo, "
+				+ "wrongAnswers=[Lunes, Martes]]",question.toString());
 	}
 
 }
