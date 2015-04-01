d867f0848c5f7dfac8b07add13ae771b52a37a56
Fernando Delgado <ikzer3@gmail.com>
2015-04-01 11:26:02 +0200

    Método para devolver todas las respuestas de una pregunta, correcta e incorrectas, mezcladas.

--- game/src/main/java/es/uniovi/asw/model/Question.java
+++ game/src/main/java/es/uniovi/asw/model/Question.java
@@ -1,8 +1,11 @@
 package es.uniovi.asw.model;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import com.google.gson.Gson;
+
+import edu.emory.mathcs.backport.java.util.Collections;
 
 public class Question {
 	
@@ -149,6 +152,20 @@ public class Question {
 		this.wrongAnswers = wrongAnswers;
 	}
 
+	/**
+	 * Devuelve todas las respuesas mezcladas
+	 * @return allAnswers
+	 */
+	public String[] getAllAnswers() {
+		String[] ans = new String[4];
+		for (int i=0;i<wrongAnswers.length;i++) 
+			ans[i] = wrongAnswers[i];
+		ans[3] = correctAnswer;
+		long s = System.nanoTime(); //seed
+		Collections.shuffle(Arrays.asList(ans), new Random(s));
+		return ans;
+	}
+	
 	/**
 	 * Devuelve el valor de vecesFallada
 	 * @return vecesFallada
